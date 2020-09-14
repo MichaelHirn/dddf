998fe96dfd780fef8ba7816b4bdcb871664755b2
Michael Hirn <michael.j.hirn@gmail.com>
2020-09-14 12:20:04 +0100

    feat(dynamo): extend dynamo functionality

--- src/repos/dynamo.repo.ts
+++ src/repos/dynamo.repo.ts
@@ -1,10 +1,18 @@
 import * as AWS from 'aws-sdk'
+import * as Rx from 'rxjs'
+import * as RxOps from 'rxjs/operators'
 import { Entity } from '../entity'
 import { IRepo } from '../repo'
 import { Result } from '../result'
 
 type SaveBatcheParams = AWS.DynamoDB.DocumentClient.BatchWriteItemInput
 type LoadBatchParams = AWS.DynamoDB.DocumentClient.BatchGetItemInput
+type ScanParams = AWS.DynamoDB.DocumentClient.ScanInput
+type ScanResponse = AWS.DynamoDB.DocumentClient.ScanOutput
+interface ParallelScanResponse {
+  scanResponse: ScanResponse
+  params: Partial<ScanParams>
+}
 
 export interface IDynamoRepoConfig {
   model: AWS.DynamoDB
@@ -27,8 +35,129 @@ export abstract class DynamoRepo<T extends Entity<any>, U extends IDynamoRepoCon
     })
   }
 
-  public abstract async saveBatch (objects: T[]): Promise<Result<void>>
-  public abstract async loadBatch (keys: string[]): Promise<Result<Map<string, Result<T>>>>
+  public abstract deserialize (dynamoItem: any): Result<T>
+
+  public abstract generateSaveBatchParams (objects: T[]): SaveBatcheParams
+
+  public abstract generateLoadBatchParams (keys: string[]): LoadBatchParams
+
+  protected generateScanParams (scanParams: Partial<ScanParams>): ScanParams {
+    return Object.assign(scanParams, { TableName: this.tableName })
+  }
+
+  public async saveBatch (objects: T[]): Promise<Result<void>> {
+    const results = await Rx.from(objects).pipe(
+      // we can have a maximum of 25 request items in a DynamoDB `batchWrite` request
+      RxOps.bufferCount(25),
+      RxOps.mergeMap(async (objectBatch) => {
+        try {
+          const params = this.generateSaveBatchParams(objectBatch)
+          await this.model.batchWrite(params).promise()
+          return Result.ok<void>()
+        } catch (error) {
+          return Result.fail<void>(error)
+        }
+      }),
+      RxOps.toArray()
+    ).toPromise()
+    if (results.every(result => result.isSuccess)) {
+      return Result.ok()
+    }
+    const failedResults = results.filter(result => result.isFailure)
+    return Result.fail(failedResults[0].error)
+  }
+
+  public async loadBatch (keys: string[]): Promise<Result<Map<string, Result<T>>>> {
+    try {
+      const responseItems = await Rx.from(keys).pipe(
+        // we can have a maximum of 100 request items in a DynamoDB `batchGet` request
+        RxOps.bufferCount(100),
+        RxOps.mergeMap(async keys => {
+          const params = this.generateLoadBatchParams(keys)
+          const response = await this.model.batchGet(params).promise()
+          const responseItems = response.Responses[this.tableName]
+          return responseItems
+        }),
+        RxOps.concatMap(responseItems => responseItems),
+        RxOps.toArray()
+      ).toPromise()
+      const map = (responseItems as any).reduce((profiles, responseObject: any) => {
+        profiles.set(
+          responseObject.entityUrn,
+          this.deserialize(responseObject)
+        )
+        return profiles
+      }, new Map<string, Result<T>>())
+      DynamoRepo.addMissingKeys<T>(keys, map)
+      return Result.ok(map)
+    } catch (e) {
+      return Result.fail(e)
+    }
+  }
+
+  public loadAll (concurrency: number = 10): Rx.Observable<T> {
+    return this._parallelScan(concurrency).pipe(
+      RxOps.expand((response: ParallelScanResponse) => {
+        const { LastEvaluatedKey } = response.scanResponse
+        if (LastEvaluatedKey instanceof Object) {
+          return this._scan({
+            ...response.params,
+            ExclusiveStartKey: LastEvaluatedKey
+          })
+        }
+        return Rx.EMPTY
+      }),
+      RxOps.mergeMap(response => {
+        const profiles = response.scanResponse.Items.map((item: any) => {
+          return this.deserialize(item).unwrap()
+        })
+        return Rx.from(profiles)
+      })
+    )
+  }
+
+  /**
+   * Make initial parallel scan requests with desired concurrency.
+   *
+   * @remarks
+   *
+   * To continue a parallel scan use {@link ProfileAggregateDynamoRepo._scan}.
+   *
+   */
+  public _parallelScan (concurrency: number, scanParams: Partial<ScanParams> = {}): Rx.Observable<ParallelScanResponse> {
+    return Rx.range(0, concurrency).pipe(
+      RxOps.mergeMap(async segment => {
+        const segmentParams = { Segment: segment, TotalSegments: concurrency }
+        const params = this.generateScanParams({...scanParams, ...segmentParams })
+        return this._scan(params)
+      }),
+      RxOps.mergeMap(response => Rx.from(response))
+    )
+  }
+
+  protected _scan (scanParams: Partial<ScanParams>): Rx.Observable<ParallelScanResponse> {
+    return Rx.defer(async () => {
+      const params = this.generateScanParams(scanParams)
+      const result = await this.model.scan(params).promise()
+      return {
+        scanResponse: result,
+        params
+      }
+    })
+  }
+
+  public async scan (startKey?: {[index: string]: string | number}): Promise<Result<T[]>> {
+    try {
+      const params = this.generateScanParams({ ExclusiveStartKey: startKey })
+      const response = await this.model.scan(params).promise()
+      const result = response.Items.map((responseObject: any) => {
+        return this.deserialize(responseObject).unwrap()
+      })
+      return Result.ok(result)
+    } catch (e) {
+      return Result.fail(e)
+    }
+  }
 
   public async save (object: T): Promise<Result<void>> {
     return await this.saveBatch([object])
