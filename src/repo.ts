f42f471bfb0ce4f73c69b0ad8b9ff0635a3f2484
Michael Hirn <michael.j.hirn@gmail.com>
2021-02-26 10:42:24 +0000

    chore: update deps

--- src/repo.ts
+++ src/repo.ts
@@ -2,6 +2,6 @@ import { Entity } from './entity'
 import { Result } from './result'
 
 export interface IRepo<T extends Entity<any>, R = void> {
-  save (t: T, args: any): Promise<Result<R>>
-  load (t: string, args: any): Promise<Result<T>>
+  save: (t: T, args: any) => Promise<Result<R>>
+  load: (t: string, args: any) => Promise<Result<T>>
 }
